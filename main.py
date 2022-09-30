5bffaf07e0088126ba1f2313e154b754357c58fe
fatema010 <fatema.sorna@gmail.com>
2022-09-30 10:48:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+URL="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+response = requests.get(URL)
+webpage = response.text
+soup = BeautifulSoup(webpage,"html.parser")
+all_movie= soup.find_all(name="h3",class_="title")
+movie_title = [movie.getText() for movie in all_movie]
+movies = movie_title[::-1]
+with open("movies.txt",mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
+
+
